996867f150111b05b252a637ba56001a7399b21a
Sammy Nava <sammy@sammynava.com>
2024-11-09 20:38:13 -0600

    Complete Overview Tutorial

---
+++ DelegateExamples/Global.cs
@@ -0,0 +1,55 @@
+﻿namespace DelegateExamples;
+
+delegate int CalculateDelegate(int operand1, int operand2);
+
+public static class Arithmetic
+{
+    public static int AddIntegerValues(int operand1, int operand2)
+    {
+        return operand1 + operand2;
+    }
+
+    public static int SubtractIntegerValues(int operand1, int operand2)
+    {
+        return operand2 - operand1;
+    }
+
+    public static int MultiplyIntegerValues(int operand1, int operand2)
+    {
+        return operand1 * operand2;
+    }
+}
+
+delegate void LogDelegate(string text);
+
+public static class UI
+{
+    public static void LogTextToConsole(string text)
+    {
+        Console.WriteLine($"static - {DateTime.Now}: {text}");
+    }
+
+    public static void LogTextToFile(string text)
+    {
+        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), true))
+        {
+            sw.WriteLine($"{DateTime.Now}: {text}");
+        }
+    }
+}
+
+public class Log
+{
+    public void LogTextToConsole(string text)
+    {
+        Console.WriteLine($"{DateTime.Now}: {text}");
+    }
+
+    public void LogTextToFile(string text)
+    {
+        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt"), true))
+        {
+            sw.WriteLine($"{DateTime.Now}: {text}");
+        }
+    }
+}
---
+++ DelegateExamples/Program.cs
@@ -0,0 +1,48 @@
+﻿using DelegateExamples;
+
+// Example One
+CalculateDelegate Add = new CalculateDelegate(Arithmetic.AddIntegerValues);
+CalculateDelegate Subtract = new CalculateDelegate(Arithmetic.SubtractIntegerValues);
+CalculateDelegate Multipy = new CalculateDelegate(Arithmetic.MultiplyIntegerValues);
+
+Console.WriteLine($"Sum = {Add(10,5)}");
+Console.WriteLine($"Difference = {Subtract(10,5)}");
+Console.WriteLine($"Product = {Multipy(10,5)}");
+
+// Example Two - Reference Static Methods
+Console.Write("Please enter your name: ");
+var name = Console.ReadLine();
+
+LogDelegate logDel = new LogDelegate(UI.LogTextToConsole); 
+logDel(name!);
+
+logDel = new LogDelegate(UI.LogTextToFile);
+logDel(name!);
+
+// Example Three - Reference Instance Methods
+Log log = new();
+
+LogDelegate logToScreen = new LogDelegate(log.LogTextToConsole);
+LogDelegate logToFile = new LogDelegate(log.LogTextToFile); 
+
+logToScreen("ExThree"!);
+logToFile("ExThree"!);
+
+// Example Four - Multicast Delegate
+LogDelegate LogTextToScreen, LogTextToFile;
+LogTextToScreen = new LogDelegate(log.LogTextToConsole);
+LogTextToFile = new LogDelegate(log.LogTextToFile);
+
+LogDelegate multiLog = LogTextToScreen + LogTextToFile;
+
+multiLog("ExFour"!);
+
+// Example Five - Passing a Delegate as a Parameter
+static void LogText(LogDelegate log, string text)
+{
+    log(text);
+}
+
+LogText(multiLog, "ExFive");
+
+
